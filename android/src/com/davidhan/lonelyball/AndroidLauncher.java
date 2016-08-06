da65ff0b667b1cdf1b26c49afda4b85eb3367f88
davjhan <mr.davidjhan@gmail.com>
2016-08-05 17:42:48 -0700

    Setup from libgdx tool

---
+++ android/src/com/davidhan/lonelyball/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.davidhan.lonelyball;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.davidhan.lonelyball.LonelyBall;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LonelyBall(), config);
+	}
+}
